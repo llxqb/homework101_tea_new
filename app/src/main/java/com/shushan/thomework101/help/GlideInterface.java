6c7878114710a743ee609087c16bb418c9a25cb1
llxqb <liuli7168165>
2019-09-07 14:31:01 +0800

    init

--- app/src/main/java/com/shushan/thomework101/help/GlideInterface.java
+++ app/src/main/java/com/shushan/thomework101/help/GlideInterface.java
@@ -9,14 +9,29 @@ import android.view.View;
  */
 
 public interface GlideInterface<T extends View> {
+    /**
+     * 展示图片
+     */
+    void displayImage(Context context, Object path, T imageView);
+    /**
+     * 展示图片
+     * 自定义默认加载中图片
+     */
     void displayImage(Context context, Object path, T imageView, int loadPic);
-
-    void displayImage(Context context, Object path, T imageView, int res, int loadPic);
-
+    /**
+     * 设置圆角图片
+     *用CircleImageView 类替代
+     */
     void displayCircularImage(Context context, Object path, T imageView, int loadPic);
-
+    /**
+     * 设置圆角图片 , 自定义圆角大小
+     *用CircleImageView 类替代
+     */
     void displayRoundedCornerImage(Context context, Object path, T imageView, Integer size, int loadPic);
 
+    /**
+     * 自适应图片
+     */
     void displayMatchImage(Context context, Object path, T imageView, int loadPic);
 
     T createImageView(Context context);
--- app/src/main/java/com/shushan/thomework101/help/ImageLoaderHelper.java
+++ app/src/main/java/com/shushan/thomework101/help/ImageLoaderHelper.java
@@ -1,19 +1,15 @@
 package com.shushan.thomework101.help;
 
 import android.content.Context;
-import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.MultiTransformation;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 import com.shushan.thomework101.mvp.utils.TranTools;
 
 import javax.inject.Inject;
-
-import jp.wasabeef.glide.transformations.BlurTransformation;
 
 /**
  * Created by li.liu on 2017/7/10.
@@ -26,6 +22,7 @@ public class ImageLoaderHelper extends GlideLoader {
     public ImageLoaderHelper(final Context ctx) {
     }
 
+    @Override
     public void displayImage(Context context, Object path, ImageView imageView) {
         RequestOptions options = new RequestOptions()
                 .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
@@ -45,18 +42,6 @@ public class ImageLoaderHelper extends GlideLoader {
         Glide.with(context).load(path).apply(options).into(imageView);
     }
 
-
-    @Override
-    public void displayImage(Context context, Object path, ImageView imageView, int res, int loadPic) {
-        RequestOptions options = new RequestOptions()
-                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
-                .error(loadPic)
-                .skipMemoryCache(true)
-                .error(res)
-                .placeholder(loadPic)
-                .dontAnimate();
-        Glide.with(context).load(path).apply(options).into(imageView);
-    }
 
     @Override
     public void displayCircularImage(Context context, Object path, ImageView imageView, int loadPic) {
@@ -82,55 +67,12 @@ public class ImageLoaderHelper extends GlideLoader {
         Glide.with(context).load(path).apply(options).into(imageView);
     }
 
-    //设置圆角毛玻璃效果
-    public void displayGlassImage(Context context, Object path, ImageView imageView, int loadPic) {
-        RequestOptions options = RequestOptions
-                .bitmapTransform(new MultiTransformation<>(new BlurTransformation(30, 3)
-//                        new RoundedCorners(TranTools.dip2px(context, 8))
-                        )
-                )// radius 越大越模糊
-                .skipMemoryCache(true)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .error(loadPic)
-                .placeholder(loadPic)
-                .dontAnimate();
-        Glide.with(context).load(path).apply(options).into(imageView);
-    }
 
     /**
      * 设置自适应图片
      */
     @Override
     public void displayMatchImage(Context context, Object path, ImageView imageView, int loadPic) {
-//        RequestOptions options = new RequestOptions()
-//                .diskCacheStrategy(DiskCacheStrategy.ALL)
-//                .error(loadPic)
-//                .skipMemoryCache(true)
-//                .placeholder(loadPic)
-//                .dontAnimate();
-//
-//        RequestListener requestListener = new RequestListener<Drawable>() {
-//            @Override
-//            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-//                return false;
-//            }
-//
-//            @Override
-//            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-//                if (imageView.getScaleType() != ImageView.ScaleType.CENTER_CROP) {
-//                    imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-//                }
-//                ViewGroup.LayoutParams params = imageView.getLayoutParams();
-//                int vw = imageView.getWidth() ;//imageView.getWidth()- imageView.getPaddingLeft() - imageView.getPaddingRight()
-//                float scale = (float) vw / (float) resource.getIntrinsicWidth();
-//                int vh = Math.round(resource.getIntrinsicHeight() * scale);
-//                params.height = vh + imageView.getPaddingTop() + imageView.getPaddingBottom();
-//                imageView.setLayoutParams(params);
-//                return false;
-//            }
-//        };
-//        Glide.with(context).load(path).listener(requestListener)
-//                .apply(options).into(imageView);
         RequestOptions options = new RequestOptions()
                 .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
                 .error(loadPic)
@@ -140,37 +82,5 @@ public class ImageLoaderHelper extends GlideLoader {
         Glide.with(context).load(path).apply(options).into(imageView);
     }
 
-
-    /**
-     * CustomizeMessageItemProvider使用到
-     * 设置圆角毛玻璃效果
-     * 静态方法
-     */
-    public static void displayGlassImage2(View context, Object path, ImageView imageView, int loadPic) {
-        RequestOptions options = RequestOptions
-                .bitmapTransform(new MultiTransformation<>(new BlurTransformation(30, 3)
-//                        new RoundedCorners(TranTools.dip2px(context, 8))
-                        )
-                )// radius 越大越模糊
-                .skipMemoryCache(true)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .error(loadPic)
-                .placeholder(loadPic)
-                .dontAnimate();
-        Glide.with(context).load(path).apply(options).into(imageView);
-    }
-
-    /**
-     * CustomizeMessageItemProvider使用到
-     */
-    public static void displayImage2(View context, Object path, ImageView imageView, int loadPic) {
-        RequestOptions options = new RequestOptions()
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .error(loadPic)
-                .skipMemoryCache(true)
-                .placeholder(loadPic)
-                .dontAnimate();
-        Glide.with(context).load(path).apply(options).into(imageView);
-    }
 
 }
